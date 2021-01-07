0e8a9a7eca7c4f72520501cc77c72c893b0836b2
Nujood <shawar.nujood@gmail.com>
2021-01-07 11:02:30 +0200

    creating the server

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import logging from './config/logging';
+import config from './config/config';
+
+// namespace to determine where our logs are coming from
+const NAMESPACE = 'Server';
+const router = express();
+
+/** Logging the request */
+router.use((req, res, next) => {
+    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
+
+    res.on('Done', () => {
+        logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${res.status}]`);
+    });
+});
+/** Parse the request */
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json())
+
+
+router.use((req, res, next) => {
+  res.header('Access-Control_Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization')
+ 
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
+    return res.status(200).json({})
+  }
+})
+
+
+/** Error Handling */
+router.use((req, res, next) => {
+  const error = new Error(' not Found :(')
+  
+  return res.status(404).json({
+    message: error.message
+  })
+})
+
+/** Create the Server */
+const httpServer = http.createServer(router)
+httpServer.listen(config.server.port,() => logging.info(NAMESPACE,  ` Server in running on ${config.server.hostName}:${config.server.port}`))
