ecf390ca3cea6d9448cc1dffaac7edf0a0e78581
Nujood <shawar.nujood@gmail.com>
2021-01-07 11:07:09 +0200

    fix the pending request by adding next() to the handlers

--- src/index.ts
+++ src/index.ts
@@ -15,32 +15,38 @@ router.use((req, res, next) => {
     res.on('Done', () => {
         logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${res.status}]`);
     });
+
+    next();
 });
+
+
 /** Parse the request */
 router.use(bodyParser.urlencoded({ extended: false }));
-router.use(bodyParser.json())
-
+router.use(bodyParser.json());
 
 router.use((req, res, next) => {
-  res.header('Access-Control_Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization')
- 
-  if (req.method == 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
-    return res.status(200).json({})
-  }
-})
+    res.header('Access-Control_Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 
 /** Error Handling */
 router.use((req, res, next) => {
-  const error = new Error(' not Found :(')
-  
-  return res.status(404).json({
-    message: error.message
-  })
-})
+    const error = new Error(' not Found :(');
+
+    return res.status(404).json({
+        message: error.message
+    });
+});
+
+
 
 /** Create the Server */
-const httpServer = http.createServer(router)
-httpServer.listen(config.server.port,() => logging.info(NAMESPACE,  ` Server in running on ${config.server.hostName}:${config.server.port}`))
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, ` Server in running on ${config.server.hostName}:${config.server.port}`));
