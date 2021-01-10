04f3198d2b4ff104c74aa1b3b0a51e336a95de1b
Nujood <shawar.nujood@gmail.com>
2021-01-10 22:24:24 +0200

    fix the pending returned promise to getBooks controller

--- src/database/queries/getBooks.ts
+++ src/database/queries/getBooks.ts
@@ -3,24 +3,12 @@ const { promisify } = require('util');
 
 import db_connection from '../config/dbConnection';
 
-type MyBooksType = {
-  title: string;
-  author: string;
-};
-
 const sql: string | QueryOptions = 'SELECT * FROM books';
 
-
 let getAllBooksQuery = async () => {
-  
-  return await db_connection.query(sql, (error: Error, results: { [key: string]: MyBooksType }, _fields) => {
-    if (error) throw new Error();
-
-    results = JSON.parse(JSON.stringify(results));  
-    console.log(results);
-    
-    return results;
-  })
+  const promiseQuery = promisify(db_connection.query).bind(db_connection);
+  let res = await promiseQuery(sql);
+  return res;
 };
 
 export { getAllBooksQuery };
